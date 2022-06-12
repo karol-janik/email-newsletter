5af1b374e03bc8b73f5ca61cf75470c602985b7d
Karol Janik <karol.janik@wpengine.com>
2022-06-12 11:42:12 +0200

    chore: use TCP LISTENER

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use newsletter::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run("127.0.0.1:8000")?.await
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,16 @@
+use std::net::TcpListener;
+
 // Inspect what code gets generated using:
 // `cargo expand --test health_check`
 #[actix_web::test]
 async fn health_check_works() {
     // arrange
-    spawn_app();
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -21,8 +21,11 @@ async fn health_check_works() {
 }
 
 // Launch our app in the background
-fn spawn_app() {
-    let server = newsletter::run("127.0.0.1:0000").expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind address");
     // Lunch the server as a background task
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
