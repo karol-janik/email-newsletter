848ba9928362c43529544b318a6d987f74b1fc6c
Karol Janik <karol.janik@wpengine.com>
2022-06-12 11:45:40 +0200

    chore: aviod expect

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     run(listener)?.await
