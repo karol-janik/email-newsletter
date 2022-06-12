f6c5a9b7fe6b88c40ce7e9873899d569edb8b5e0
Karol Janik <karol.janik@wpengine.com>
2022-06-12 11:27:45 +0200

    refactoring(tests): bind app to random port

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use newsletter::run;
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    run("127.0.0.1:8000")?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@
 async fn health_check_works() {
     // arrange
     spawn_app();
-    // We need to brin in `reqwest`
+    // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
@@ -22,7 +22,7 @@ async fn health_check_works() {
 
 // Launch our app in the background
 fn spawn_app() {
-    let server = newsletter::run().expect("Failed to bind address");
+    let server = newsletter::run("127.0.0.1:0000").expect("Failed to bind address");
     // Lunch the server as a background task
     let _ = tokio::spawn(server);
 }
