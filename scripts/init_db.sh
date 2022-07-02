9a160787c32e47974e57e9d03916ba3be520408b
karol-janik <karol.janik@wpengine.com>
2022-07-02 21:33:49 +0200

    feat: update init db script, add first migration script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,16 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error psql is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error sqlx is not installed."
+    exit 1
+fi
+
 
 # Check if a custom huser has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
@@ -10,7 +20,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=5433}"
 
 # Lunch postgres using Docker
 docker run \
@@ -21,3 +31,12 @@ docker run \
     -d postgres \
     postgres -N 1000
     # ^ Increased maximum number of connections for testing purposes
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavilable - sleeping"
+    sleep 1
+done
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
