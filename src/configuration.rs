7daa28e8f8bc449d58d3e05aa68226045369b961
karol-janik <karol.janik@wpengine.com>
2022-08-21 15:48:14 +0200

    feat: load config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,3 @@
-
-
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
@@ -13,4 +11,25 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialise our configuration reader
+    let config = config::Config::builder()
+        .add_source(config::File::with_name("configuration.yaml"))
+        .build()
+        .unwrap();
+
+    // Try to convert the configuration values it read into
+    // our settings type
+    config.try_deserialize()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres//{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
